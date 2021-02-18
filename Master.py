4d4670d0465cff3bb96782f296da9709029705f0
d-g-brick <47373750+d-g-brick@users.noreply.github.com>
2021-02-17 18:21:42 -0600

    Create Master.py

---
+++ Master.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 18:11:43 2021
+
+@author: Declan Brick, Matthew Bethea
+
+
+Master script file for Project 1 of MAE 343
+"""
+
+print('Hello World')
