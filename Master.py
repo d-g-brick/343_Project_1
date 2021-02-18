1662852182024922b1303a8eb909820b638df856
d-g-brick <47373750+d-g-brick@users.noreply.github.com>
2021-02-17 19:20:59 -0600

    Update Master.py

--- Master.py
+++ Master.py
@@ -13,16 +13,23 @@ import Theta_Calc
 
 
 # Function (gamma, M, theta, beta)
-# First determines whatever is needed of M,theta, or beta using the eq's/methods from 2,4, and 5 
-# uses these values and the flow eq's to determine the Mn1, Mn2, M2, p ratio, T ratio
-#
+def Func_Name(gamma, M, Theta, Beta):#need a better name
+    
+
+
+    # First determines whatever is needed of M,theta, or beta using the eq's/methods from 2,4, and 5 
+    # uses these values and the flow eq's to determine the Mn1, Mn2, M2, p ratio, T ratio
+    #
 
 
 
 
 
-# This section is the Mn1, Mn2, etc.
+    # This section is the Mn1, Mn2, etc. 
+    Mn1=M*math.sin(math.radians(Beta))
+    Mn2=((Mn1**2+(2/(gamma-1)))/((2*gamma/(gamma-1)*Mn1**2)-1))**0.5
 
+    return #add what need to return here 
 
 
  
