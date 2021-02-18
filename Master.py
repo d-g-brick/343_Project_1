b0a8a3d1f2b8b4b9d3a2e6562b884253202daa5c
d-g-brick <47373750+d-g-brick@users.noreply.github.com>
2021-02-17 19:13:55 -0600

    Update Master.py

--- Master.py
+++ Master.py
@@ -11,12 +11,20 @@ import math
 import Theta_Calc
 
 
-print('Hello World')
-
 
 # Function (gamma, M, theta, beta)
 # First determines whatever is needed of M,theta, or beta using the eq's/methods from 2,4, and 5 
 # uses these values and the flow eq's to determine the Mn1, Mn2, M2, p ratio, T ratio
-# 
+#
+
+
+
+
+
+# This section is the Mn1, Mn2, etc.
+
+
+
+ 
 # Function for the Cp stuff
 # Potato
