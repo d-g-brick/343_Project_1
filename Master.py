9b84a3e6dcd08abb2c66654b9f3b611832978fba
d-g-brick <47373750+d-g-brick@users.noreply.github.com>
2021-02-17 18:27:48 -0600

    Update Master.py

--- Master.py
+++ Master.py
@@ -9,3 +9,10 @@ Master script file for Project 1 of MAE 343
 """
 
 print('Hello World')
+
+
+# Function (gamma, M, theta, beta)
+# First determines whatever is needed of M,theta, or beta using the eq's/methods from 2,4, and 5 
+# uses these values and the flow eq's to determine the Mn1, Mn2, M2, p ratio, T ratio
+# 
+# Function for the Cp stuff
