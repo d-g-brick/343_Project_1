a802d597b4ae776221ca6b8f62ff1db8e17562d4
d-g-brick <47373750+d-g-brick@users.noreply.github.com>
2021-02-17 18:55:32 -0600

    Update Theta_Calc.py

--- Theta_Calc.py
+++ Theta_Calc.py
@@ -5,4 +5,22 @@ Created on Wed Feb 17 18:50:47 2021
 @author: Declan Brick
 """
 def ThetaCalc(g,M,Beta):
+    """
+    Calculates flow deflection angle using Equation 1 in the Project Description
+
+    Parameters
+    ----------
+    g : Float
+        Ratio of specific heats
+    M : Float
+        Free Stream Mach Number before Shock
+    Beta : Float
+        Oblique Shock Angle, in degrees 
+
+    Returns
+    -------
+    Theta: Float
+        Flow Deflection Angle, in degrees
+
+    """
     
