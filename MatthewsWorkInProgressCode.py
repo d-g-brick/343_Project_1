39f359fde7079082a4182ee6f1b06eaa3a658490
Frozeneclipse10 <79233530+Frozeneclipse10@users.noreply.github.com>
2021-02-19 17:08:08 -0600

    Update MatthewsWorkInProgressCode.py

--- MatthewsWorkInProgressCode.py
+++ MatthewsWorkInProgressCode.py
@@ -13,7 +13,7 @@ import math
 def mach(g,b,t):
     B = math.radians(b)
     T = math.radians(t)
-    Mach = math.sqrt((2*math.tan(T)-2*(1/(math.tan(B)))/(g*math.tan(T)+math.tan(T)*math.cos(2*B)-2*math.sin(B)*math.sin(B)*(1/(math.tan(B))))))
+    Mach = math.sqrt((-2*math.tan(T)-2*(1/(math.tan(B)))/(g*math.tan(T)+math.tan(T)*math.cos(2*B)-2*math.sin(B)*math.sin(B)*(1/(math.tan(B))))))
     return Mach
 
 def theta(g,M,B):
@@ -44,7 +44,7 @@ def theta(g,M,B):
     #this was verified using a Ti-84 to output the correct answer, but many want to change output decimal precision
     
 
-def S_beta(M):
+def beta(M):
 
     #using the functions for finding Beta max (the maximum shock angle) supported by the maximum Theta (the maximum flow deflection
     Theta_max = M_Theta(M, (M_Beta(g,M)))
@@ -85,54 +85,6 @@ def S_beta(M):
             print("Number of iteration to solution: ", n)
             print("solution found: ")
             
-        else:
-            
-            print("failed")
-            return None
-
-    return Old_A - f(Old_A)*(Old_B - Old_A)/(f(Old_B) - f(Old(A)))
-
-
-def W_beta(f,Ma,Tm):
-
-    #using the functions for finding Beta max (the maximum shock angle) supported by the maximum Theta (the maximum flow deflection
-    Theta_max = M_Theta(M, (M_Beta(g,M)))
-
-        #Strong Root for Beta function
-        #This root will be between 90 and the theta max
-        #F = the function
-        #sA = Mach angle
-        #sB = Theta max
-        #iteration is set automatically to 100
-
-    #verifying
-    if f(Ma)*f(Tm) >= 0:
-        return None
-
-    #setting bounds
-    Old_A = 
-    Old_B = Theta_max
-
-    #iterating to find the solution for beta
-    for n in range(1, 100):
-        
-        new = Old_A - f(Old_A)*(Old_B - Old_A)/(f(Old_B)-f(Old_A))
-        solvenew = f(new)
-        
-        if f(Old_A)*solvenew <0:
-            
-            Old_A = Old_A
-            Old_B = new
-            
-        elif f(Old_A)*solvenew <0:
-            
-            Old_A = new
-            Old_B = Old_B
-            
-        elif solvenew == 0:
-            
-            print("Number of iteration to solution: ", n)
-            print("solution found: ")
             
         else:
             
@@ -140,28 +92,47 @@ def W_beta(f,Ma,Tm):
             return None
 
     return Old_A - f(Old_A)*(Old_B - Old_A)/(f(Old_B) - f(Old(A)))
-
 
 
 
 def M_Beta(g,M):
+    
+    f1 = 1+((g-1)/2)*M**2+((g+1)/16)*M**4
+    f2 = math.sqrt((g+1)*f1)
+    f3 = (((y+1)/4)*M**2+f2-1)
+    f4 = math.sqrt((1/(g*(M**2)))*f3)
+    BM = 1/math.sin(f4)
+    
+    return BM
 
-    return results
+def M_Theta(gamma, Mach, Beta):
+    
+    g = gamma
+    M = math.radians(Mach)
+    B = math.radians(Beta)
+    
+    top = (((M**2)*(math.sin(B-1)*math.sin(B-1)))*(1/math.tan(B)))
+    bot = ((1/2)*(g+1)*(M**2)-(M**2)*(math.sin(B)*math.sin(B))+1)
+    
+    TM = 1/math.tan(top/bot)
+    
+    return TM
 
-def M_Theta(M,B):
 
-    return results
+def Mach_a(Mach):
+    
+    M = math.radians(Mach)
+    
+    mu = 1/(math.sin(1/M))
+    
+    return Mu
 
-def Mach_a():
-
-    return results
 
 functionMatrix = {
   "mach":mach,
   "theta":theta,
   "beta":beta,
 }
-
 
 
 print("Available Functions:")
@@ -171,12 +142,12 @@ for function in functionMatrix.keys():
 chosen = input("Which Function Would You Like to run?").lower()
 print(chosen)
 
-g = float(input("Gamma: "))
+#g = float(input("Gamma: "))
 
-if chosen in ["Mach","mach"]:
-    b = float(input("Beta: "))
-    t = float(input("Theta: "))
-    print("Output =",functionMatrix[chosen](g,b,t))
+    print("Output =",functionMatrix[chosen]())
+
+
+
 
 
 """
