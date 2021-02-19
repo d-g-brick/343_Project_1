0deb1c210bf5e6da3ddc250d7143ec8ce07ce694
d-g-brick <47373750+d-g-brick@users.noreply.github.com>
2021-02-19 15:50:38 -0600

    Update Theta_Calc.py

--- Theta_Calc.py
+++ Theta_Calc.py
@@ -10,10 +10,7 @@ Project 1
 import math
 
 
-def Mach(g,M,T):
-    return
-
-def Theta(g,M,B):
+def Theta(g,M,Beta):
     
     """
     Calculates flow deflection angle using Equation 1 in the Project Description
@@ -39,51 +36,3 @@ def Theta(g,M,B):
     
     return Theta
     #this was verified using a Ti-84 to output the correct answer, but many want to change output decimal precision
-    
-
-def Beta(f,M,T,N):
-    if f(M)*f(T) >= 0:
-        return None
-    Old_M = M
-    Old_T = T
-    for n in range(1, N+1):
-        store = Old_M - f(Old_M)*(Old_T - Old_M)/(f(Old_T)-f(Old_M))
-        solvestore = f(store)
-        if f(Old_M)*solvestore <0:
-            Old_M = Old_M
-            Old_T = store
-        elif f(Old_T)*solvestore <0:
-            Old_M = store
-            Old_T = Old_T
-        elif solvestore = 0:
-            print("solution found")
-        else:
-            print("failed")
-            return None
-    return Old_M - f(Old_M)*(Old_T - Old_M)/(f(Old_T) - f(Old(M)))
-
-
-
-functionMatrix = {
-  "square":square,
-  "cube":cube,
-  "inverse":inverse,
-}
-
-print("Available Functions:")
-for function in functionMatrix.keys():
-  print(function)
-
-chosen = input("Which Function Would You Like to run?").lower()
-value = float(input("X: "))
-print("Output =",functionMatrix[chosen](value))
-
-
-"""
-Mn1=M*math.sin(math.radians(Beta))
-Mn2=((Mn1**2+(2/(gamma-1)))/((2*gamma/(gamma-1)*Mn1**2)-1))**0.5
-M2=Mn2/(math.sin(math.radians(Beta-Theta)))
-Pressure_Ratio=1+(Mn1**2-1)*(2*gamma/(gamma+1))
-Density_Ratio=((gamma+1)*Mn1**2)/((gamma-1)*Mn1**2+2)
-Temperature_ratio=Pressure_Ratio/Density_Ratio
-"""
